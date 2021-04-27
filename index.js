0b54d06d6d66781d04267f68c1655d99eb27e49b
Maks Dundukov <mdundukov@gmail.com>
2021-04-28 01:09:49 +0300

    fix secure link

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let dataBase = [];
-fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c \n')
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c')
     .then(response => response.json())
     .then(data => dataBase = data.results);
 
@@ -15,7 +15,7 @@ setTimeout(() => clearInterval(timeId), 3000);
 
 function createCardImage(movie) {
     const {original_title, backdrop_path} = movie;
-    const poster_url = 'http://image.tmdb.org/t/p/w342' + `${backdrop_path}`;
+    const poster_url = 'https://image.tmdb.org/t/p/w342' + `${backdrop_path}`;
     const img = document.createElement('img');
 
     img.setAttribute('src', poster_url);
