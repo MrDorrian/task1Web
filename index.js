c40d6b3f8609422e78656809dbb65c7040aa485c
Maks Dundukov <mdundukov@gmail.com>
2021-04-27 23:47:41 +0300

    fix some files

---
+++ index.js
@@ -0,0 +1,5 @@
+let dataBase = {};
+fetch('http://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c \n')
+    .then(response => response.json())
+    .then(data => dataBase = data);
+
