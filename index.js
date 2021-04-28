8ef5a7bd2498f9470bc5c92b9dfcef90acfec172
MrDorrian <mdundukov@gmail.com>
2021-04-28 14:25:18 +0300

    FIx

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict'
-const BASE_URL_DATA = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US&page=2';
+const API_DATA = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US&page=';
+let CURRENT_PAGE = 1;
+const BASE_URL_DATA = `${API_DATA}` + `${CURRENT_PAGE}`;
+
 const movies = document.getElementById('root');
+let newMovie = [];
 
 function getData(url) {
     return fetch(url);
-
 }
 
 function createMovies(data) {
-    const newMovie = data.results.map((movie) => createCardImage(movie));
+    newMovie = [];
+    movies.innerText = '';
+    newMovie = data.results.map((movie) => createCardImage(movie));
     movies.append(...newMovie);
-    console.log(data);
 }
 
 function createCardImage(movie) {
@@ -38,3 +42,18 @@ getData(BASE_URL_DATA)
 const btnPrev = document.getElementById('prev');
 const btnNext = document.getElementById('next');
 
+btnPrev.addEventListener('click', () => {
+    if (CURRENT_PAGE >= 2) {
+        CURRENT_PAGE--;
+        getData(API_DATA + CURRENT_PAGE)
+            .then(response => response.json())
+            .then(data => createMovies(data));
+    }
+});
+
+btnNext.addEventListener('click', () => {
+    ++CURRENT_PAGE;
+    getData(API_DATA + CURRENT_PAGE)
+        .then(response => response.json())
+        .then(data => createMovies(data));
+});
