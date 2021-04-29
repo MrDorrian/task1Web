34ff1041cc0f0630498d351861cf302bd6c8c246
MrDorrian <mdundukov@gmail.com>
2021-04-29 14:59:47 +0300

    add files api.js and buttons.js

---
+++ assets/api/api.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const API_DATA = 'https://api.themoviedb.org/3/movie/now_playing?api_key=ebea8cfca72fdff8d2624ad7bbf78e4c&language=en-US&page=';
+let CURRENT_PAGE = 1;
+const BASE_URL_DATA = `${API_DATA}` + `${CURRENT_PAGE}`;
---
+++ assets/buttons/buttons.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const firstBtn = document.getElementById('firstBtn');
+const lastBtn = document.getElementById('lastBtn');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+const changeBtn = document.getElementById('pagination');
+
+
+function buttonsPagin(totalDB) {
+    const {total_pages} = totalDB;
+    const pagination = document.getElementById('pagination');
+    const divPagination = document.createElement('div');
+
+    for (let i = 1; i <= total_pages; i++) {
+        divPagination.classList.add('pagination-buttons', 'display-flex-buttons');
+        divPagination.textContent = CURRENT_PAGE;
+        pagination.append(divPagination);
+        i++;
+    }
+}
+
+changeBtn.addEventListener('click', (e) => {
+    const {target: {innerText}} = e;
+    getData(API_DATA + innerText)
+        .then(response => response.json())
+        .then(data => createMovies(data));
+})
+
+firstBtn.addEventListener('click', () => {
+    getData(BASE_URL_DATA)
+        .then(response => response.json())
+        .then(data => createMovies(data));
+})
+
+lastBtn.addEventListener('click', () => {
+    const {total_pages} = totalDB;
+    CURRENT_PAGE = total_pages;
+    getData(API_DATA + total_pages)
+        .then(response => response.json())
+        .then(data => {
+            createMovies(data);
+        });
+})
+
+prevBtn.addEventListener('click', () => {
+    if (CURRENT_PAGE >= 2) {
+        CURRENT_PAGE--;
+        getData(API_DATA + CURRENT_PAGE)
+            .then(response => response.json())
+            .then(data => createMovies(data));
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    ++CURRENT_PAGE;
+    getData(API_DATA + CURRENT_PAGE)
+        .then(response => response.json())
+        .then(data => createMovies(data));
+});
