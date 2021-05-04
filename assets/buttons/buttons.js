d34d5302d007eda2da5a0be32367e0360c154893
Maks Dundukov <mdundukov@gmail.com>
2021-05-04 21:53:44 +0300

    Add Pagination Buttons

--- assets/buttons/buttons.js
+++ assets/buttons/buttons.js
@@ -4,11 +4,14 @@ const changeBtn = document.getElementById('paginationBtn');
 const firstBtn = document.getElementById('firstBtn');
 const prevBtn = document.getElementById('prevBtn');
 
+const newArrPagination = [];
+const newPagination = [];
+
 function dataButtons(data) {
     const {page, total_pages} = data;
 
     for (let i = page; i <= total_pages; i++) {
-        createPagination(i);
+        newPagination.push(i);
         if (i >= total_pages) {
             pagination.append(createNextBtn, createLastBtn);
             const lastBtn = document.getElementById('lastBtn');
@@ -38,24 +41,52 @@ function dataButtons(data) {
     }
 }
 
-function createPagination(i) {
-    const divPagination = document.createElement('div');
-    divPagination.classList.add('pagination-buttons',);
-    divPagination.textContent = i;
-    createPaginationBtn.append(divPagination);
-
+for (let i = CURRENT_PAGE; i <= CURRENT_PAGE + 4; i++) {
+    newArrPagination.push(i);
 }
+
+function createPagination(newArr) {
+    if (createPaginationBtn.firstChild) {
+        for (let i = 0; i <= 4; i++) {
+            createPaginationBtn.firstChild.remove();
+        }
+    }
+    newArr.map(pagin => {
+        const divPagination = document.createElement('div');
+        divPagination.classList.add('pagination-buttons',);
+        divPagination.textContent = pagin;
+        createPaginationBtn.append(divPagination);
+    })
+}
+
+function upgradePagination(CURRENT_PAGE) {
+    if (CURRENT_PAGE + 4 <= newPagination.length) {
+        for (let i = CURRENT_PAGE; i <= CURRENT_PAGE + 4; i++) {
+            newArrPagination.push(i);
+            newArrPagination.shift(i);
+        }
+    }
+}
+
+
+createPagination(newArrPagination);
 
 changeBtn.addEventListener('click', (e) => {
     const {target: {innerText}} = e;
-    CURRENT_PAGE = innerText;
-    getData(API_DATA + innerText)
-        .then(response => response.json())
-        .then(data => createMovies(data));
+    CURRENT_PAGE = Number(innerText);
+    if (CURRENT_PAGE <= newPagination.length) {
+        getData(API_DATA + innerText)
+            .then(response => response.json())
+            .then(data => {
+                createMovies(data);
+                upgradePagination(CURRENT_PAGE);
+                createPagination(newArrPagination);
+            });
+    }
 });
 
+
 firstBtn.addEventListener('click', () => {
-    CURRENT_PAGE = 1;
     getData(BASE_URL_DATA)
         .then(response => response.json())
         .then(data => createMovies(data));
